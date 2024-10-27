50786d589b9c747096243a09f62c080d8cd3dda5
Ivan Semikin <i.semikin.94@icloud.com>
2024-10-27 15:35:50 +0100

    Created HomeView with new directory

---
+++ DestinyMatrix/Screens/Home/HomeView.swift
@@ -0,0 +1,18 @@
+//
+//  HomeView.swift
+//  DestinyMatrix
+//
+//  Created by Иван Семикин on 27/10/2024.
+//
+
+import SwiftUI
+
+struct HomeView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    HomeView()
+}
