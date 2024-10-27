a844b8a4599f0bb2a585ce2ae51a6281f6abcb83
Ivan Semikin <i.semikin.94@icloud.com>
2024-10-27 15:19:27 +0100

    Initial Commit

---
+++ DestinyMatrix/DestinyMatrixApp.swift
@@ -0,0 +1,17 @@
+//
+//  DestinyMatrixApp.swift
+//  DestinyMatrix
+//
+//  Created by Иван Семикин on 27/10/2024.
+//
+
+import SwiftUI
+
+@main
+struct DestinyMatrixApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
