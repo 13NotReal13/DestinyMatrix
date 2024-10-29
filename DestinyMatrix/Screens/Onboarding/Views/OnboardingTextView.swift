7cd81b95ca05c394976edad0576fee434bcc230e
Ivan Semikin <i.semikin.94@icloud.com>
2024-10-29 19:09:25 +0100

    Created OnboardingTextView

---
+++ DestinyMatrix/Screens/Onboarding/Views/OnboardingTextView.swift
@@ -0,0 +1,18 @@
+//
+//  OnboardingTextView.swift
+//  DestinyMatrix
+//
+//  Created by Иван Семикин on 29/10/2024.
+//
+
+import SwiftUI
+
+struct OnboardingTextView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    OnboardingTextView()
+}
