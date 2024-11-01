b6aa234edaf5f059fc961c77b65e0bae0287628b
Ivan Semikin <i.semikin.94@icloud.com>
2024-11-01 14:41:27 +0100

    Created DatePickerView

--- DestinyMatrix/Screens/EnterData/EnterDataView.swift
+++ DestinyMatrix/Screens/EnterData/EnterDataView.swift
@@ -13,6 +13,13 @@ struct EnterDataView: View {
     
     @State private var isDatePickerPresented = false
     
+    private var dateFormatter: DateFormatter {
+        let formatter = DateFormatter()
+        formatter.dateStyle = .medium
+        formatter.locale = Locale(identifier: "ru")
+        return formatter
+    }
+    
     var body: some View {
         ZStack {
             AnimatedStarryBackgroundView()
@@ -32,45 +39,11 @@ struct EnterDataView: View {
                 }
                 
                 VStack {
-                    Text("Выберите вашу дату рождения")
-                        .customText(fontSize: 12, textColor: .white)
-                    
-                    Button(action: {
-                        isDatePickerPresented.toggle()
-                    }) {
-                        HStack {
-                            Text(dateFormatter.string(from: dateBirthday))
-                                .foregroundStyle(.black)
-                                .shadow(color: .purple, radius: 5)
-                                .frame(width: UIScreen.main.bounds.width * 0.55)
-                        }
-                        .customButtonStyle(color1: .lightAroundSphere, color2: .lightAroundSphere, shape: .capsule)
-                    }
-                    .sheet(isPresented: $isDatePickerPresented) {
-                        ZStack {
-                            AnimatedStarryBackgroundView()
-                            
-                            VStack {
-                                DatePicker("Дата рождения", selection: $dateBirthday, displayedComponents: .date)
-                                    .environment(\.locale, Locale(identifier: "ru"))
-                                    .datePickerStyle(.wheel)
-                                    .colorInvert()
-                                    .colorMultiply(Color.white)
-                                    .labelsHidden()
-                                    .padding()
-                                
-                                Button {
-                                    isDatePickerPresented = false
-                                } label: {
-                                    Text("Готово")
-                                        .padding(.horizontal)
-                                        .customText(fontSize: 16, textColor: .white)
-                                        .customButtonStyle(color1: .backgroundColor1, color2: .buttonColor2, shape: .capsule)
-                                }
-                            }
-                        }
-                    }
-                    .foregroundStyle(.gray)
+                    DatePickerView(
+                        isDatePickerPresented: $isDatePickerPresented,
+                        dateBirthday: $dateBirthday,
+                        dateFormatter: dateFormatter
+                    )
                 }
                 
                 Spacer()
@@ -87,13 +60,6 @@ struct EnterDataView: View {
                 Spacer()
             }
         }
-    }
-    
-    private var dateFormatter: DateFormatter {
-        let formatter = DateFormatter()
-        formatter.dateStyle = .medium
-        formatter.locale = Locale(identifier: "ru")
-        return formatter
     }
 }
 
--- DestinyMatrix/Screens/EnterData/Views/DatePickerView.swift
+++ DestinyMatrix/Screens/EnterData/Views/DatePickerView.swift
@@ -5,14 +5,71 @@
 //  Created by Иван Семикин on 01/11/2024.
 //
 
+import Foundation
 import SwiftUI
 
 struct DatePickerView: View {
+    @Binding var isDatePickerPresented: Bool
+    @Binding var dateBirthday: Date
+    @State var dateFormatter: DateFormatter
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        ZStack {
+            AnimatedStarryBackgroundView()
+            
+            VStack {
+                Text("Выберите вашу дату рождения")
+                    .customText(fontSize: 12, textColor: .white)
+                
+                Button(action: {
+                    isDatePickerPresented.toggle()
+                }) {
+                    HStack {
+                        Text(dateFormatter.string(from: dateBirthday))
+                            .foregroundStyle(.black)
+                            .shadow(color: .purple, radius: 5)
+                            .frame(width: UIScreen.main.bounds.width * 0.55)
+                    }
+                    .customButtonStyle(color1: .lightAroundSphere, color2: .lightAroundSphere, shape: .capsule)
+                }
+                .sheet(isPresented: $isDatePickerPresented) {
+                    ZStack {
+                        AnimatedStarryBackgroundView()
+                        
+                        VStack {
+                            DatePicker("Дата рождения", selection: $dateBirthday, displayedComponents: .date)
+                                .environment(\.locale, Locale(identifier: "ru"))
+                                .datePickerStyle(.wheel)
+                                .colorInvert()
+                                .colorMultiply(Color.white)
+                                .labelsHidden()
+                                .padding()
+                            
+                            Button {
+                                isDatePickerPresented = false
+                            } label: {
+                                Text("Готово")
+                                    .padding(.horizontal)
+                                    .customText(fontSize: 16, textColor: .white)
+                                    .customButtonStyle(color1: .backgroundColor1, color2: .buttonColor2, shape: .capsule)
+                            }
+                        }
+                    }
+                }
+                .foregroundStyle(.gray)
+            }
+        }
     }
 }
 
 #Preview {
-    DatePickerView()
+    DatePickerView(
+        isDatePickerPresented: .constant(false),
+        dateBirthday: .constant(Date()),
+        dateFormatter: {
+            let formatter = DateFormatter()
+            formatter.dateStyle = .medium
+            formatter.locale = Locale(identifier: "ru")
+            return formatter
+        }())
 }
