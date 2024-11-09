f7265dd54e6173149cd9048fa885382bf15b531e
Ivan Semikin <i.semikin.94@icloud.com>
2024-11-09 12:09:11 +0100

    Updated MatrixData model

--- DestinyMatrix/Models/MatrixData.swift
+++ DestinyMatrix/Models/MatrixData.swift
@@ -9,20 +9,59 @@ import Foundation
 
 typealias ArkanDictionary = [Int: ArkanInfo]
 
-struct LifeNumbers {
-    var destinyNumber: ArkanDictionary
-    var lifePathNumber: ArkanDictionary
-    var soulNumber: ArkanDictionary
-    var karmaNumber: ArkanDictionary
-    var personalityNumber: ArkanDictionary
-}
-
 struct MatrixData {
     var lifeNumbers: LifeNumbers
-    var karmicKnots: ArkanDictionary
-    var resourcesAndTalents: ArkanDictionary
-    var emotionalAndPersonalTraits: ArkanDictionary
-    var areasOfInfluenceAndDevelopment: ArkanDictionary
-    var recommendations: ArkanDictionary
-    var energyFlows: ArkanDictionary
+    var karmicKnots: ArkanCategory
+    var resourcesAndTalents: ArkanCategory
+    var emotionalAndPersonalTraits: ArkanCategory
+    var areasOfInfluenceAndDevelopment: ArkanCategory
+    var recommendations: ArkanCategory
+    var energyFlows: ArkanCategory
+}
+
+struct LifeNumbers {
+    let сategoryTitle: String = "Жизненные Числа"
+    var destinyNumber: ArkanCategory
+    var lifePathNumber: ArkanCategory
+    var soulNumber: ArkanCategory
+    var karmaNumber: ArkanCategory
+    var personalityNumber: ArkanCategory
+}
+
+struct ArkanCategory {
+    var сategoryTitle: String
+    var arkanInfo: ArkanDictionary
+}
+
+struct ArkanInfo {
+    let title: String
+    let shortDescription: String
+    let mainDescription: String
+    
+    let positiveEnergyTitle: String = "Энергия в плюсе:"
+    private let positiveEnergyText: String
+    var positiveEnergy: String {
+        "Когда ваша энергия наполнена силой: \(positiveEnergyText)"
+    }
+
+    let negativeEnergyTitle: String = "Энергия в минусе:"
+    private let negativeEnergyText: String
+    var negativeEnergy: String {
+        "Когда ваша энергия ослабевает: \(negativeEnergyText)"
+    }
+    
+    let adviceTitle: String = "Советы для восстановления энергии:"
+    private let adviceText: String
+    var advice: String {
+        "Чтобы гармонизировать вашу энергию: \(adviceText)"
+    }
+    
+    init(title: String, shortDescription: String, mainDescription: String, positiveEnergyText: String, negativeEnergyText: String, adviceText: String) {
+        self.title = title
+        self.shortDescription = shortDescription
+        self.mainDescription = mainDescription
+        self.positiveEnergyText = positiveEnergyText
+        self.negativeEnergyText = negativeEnergyText
+        self.adviceText = adviceText
+    }
 }
