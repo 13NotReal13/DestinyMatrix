e7de33c4a2add6c559cac645f82e739b053de26b
Ivan Semikin <i.semikin.94@icloud.com>
2024-11-12 19:06:27 +0100

    Created ProfessionsAndRolesView

---
+++ DestinyMatrix/Screens/Matrix/Views/ProfessionsAndRolesView.swift
@@ -0,0 +1,18 @@
+//
+//  ProfessionsAndRolesView.swift
+//  DestinyMatrix
+//
+//  Created by Иван Семикин on 12/11/2024.
+//
+
+import SwiftUI
+
+struct ProfessionsAndRolesView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ProfessionsAndRolesView()
+}
