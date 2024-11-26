47ec7b19129c8bd48071086dd995842e7a70e7a0
Ivan Semikin <i.semikin.94@icloud.com>
2024-11-26 20:31:43 +0100

    Created createPDF in PDFManager

--- DestinyMatrix/Services/PDFManager.swift
+++ DestinyMatrix/Services/PDFManager.swift
@@ -13,5 +13,22 @@ final class PDFManager {
     
     private init() {}
     
-    
+    func createPDF(from scrollView: UIScrollView) -> Data? {
+        let originalFrame = scrollView.frame
+        
+        let contentSize = scrollView.contentSize
+        scrollView.frame = CGRect(origin: .zero, size: contentSize)
+        
+        let pdfData = NSMutableData()
+        UIGraphicsBeginPDFContextToData(pdfData, CGRect(origin: .zero, size: contentSize), nil)
+        UIGraphicsBeginPDFPage()
+        
+        guard let pdfContext = UIGraphicsGetCurrentContext() else { return nil }
+        scrollView.layer.render(in: pdfContext)
+        
+        UIGraphicsEndPDFContext()
+        
+        scrollView.frame = originalFrame
+        return pdfData as Data
+    }
 }
