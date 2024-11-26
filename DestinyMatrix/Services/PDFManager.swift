bd30faa9e2bb5a80b3370abe2fd39234b84fb06e
Ivan Semikin <i.semikin.94@icloud.com>
2024-11-26 20:26:44 +0100

    Created PDFManager

---
+++ DestinyMatrix/Services/PDFManager.swift
@@ -0,0 +1,17 @@
+//
+//  PDFManager.swift
+//  DestinyMatrix
+//
+//  Created by Иван Семикин on 26/11/2024.
+//
+
+import Foundation
+import UIKit
+
+final class PDFManager {
+    static let shared = PDFManager()
+    
+    private init() {}
+    
+    
+}
