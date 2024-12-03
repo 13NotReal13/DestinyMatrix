b909513c785da7e5ee2edea9cd3e320288defc0d
Ivan Semikin <i.semikin.94@icloud.com>
2024-12-03 20:39:21 +0100

    Created new models MatrixDataNew

---
+++ DestinyMatrix/ModelsNew/DataModelNew/MatrixDataNew.swift
@@ -0,0 +1,8 @@
+//
+//  MatrixDataNew.swift
+//  DestinyMatrix
+//
+//  Created by Иван Семикин on 02/12/2024.
+//
+
+import Foundation
---
+++ DestinyMatrix/ModelsNew/MatrixDataNew.swift
@@ -0,0 +1,31 @@
+//
+//  MatrixDataNew.swift
+//  DestinyMatrix
+//
+//  Created by Иван Семикин on 02/12/2024.
+//
+
+import Foundation
+
+struct MatrixDataNew {
+    var name: String
+    var dateOfBirthday: Date
+    var dateCreationMatrix: Date
+    
+    var destinyNumber: ArkanInfoNew // Число Судьбы
+    var lifePath: ArkanInfoNew // Жизненный Путь
+    var relationships: ArkanInfoNew // Отношения
+    var finances: ArkanInfoNew // Деньги и Финансы
+    var career: ArkanInfoNew // Карьера и Профессия
+    var health: ArkanInfoNew // Здоровье
+    var talents: ArkanInfoNew // Таланты и Способности
+    var challenges: ArkanInfoNew // Испытания и Препятствия
+    var personalGrowth: ArkanInfoNew // Личностный Рост
+    var innerSelf: ArkanInfoNew // Внутренний Мир
+    var purpose: ArkanInfoNew // Предназначение
+    var karmicLessons: ArkanInfoNew // Кармические Уроки
+    var futureProspects: ArkanInfoNew // Будущее
+}
+
+typealias ArkanDictionaryNew = [Int: ArkanInfo]
+
