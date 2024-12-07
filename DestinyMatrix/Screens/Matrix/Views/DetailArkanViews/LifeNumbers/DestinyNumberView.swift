25be875aec6c825d4da13ef5a4af6122753967e2
Ivan Semikin <i.semikin.94@icloud.com>
2024-12-07 17:48:21 +0100

    Added data for RelationshipsArkans

--- DestinyMatrix/Screens/Matrix/Views/DetailArkanViews/LifeNumbers/DestinyNumberView.swift
+++ DestinyMatrix/Screens/Matrix/Views/DetailArkanViews/LifeNumbers/DestinyNumberView.swift
@@ -12,10 +12,10 @@ struct DestinyNumberView: View {
     
     var body: some View {
         VStack(alignment: .leading, spacing: 16) {
-            Text(LifePathArkans.allArkans[1]!.title)
+            Text(RelationshipsArkans.allArkans[1]!.title)
                 .font(.title)
             
-            ForEach(LifePathArkans.allArkans[1]!.subcategories, id: \.self) { subcategorie in
+            ForEach(RelationshipsArkans.allArkans[1]!.subcategories, id: \.self) { subcategorie in
                 Text(subcategorie.title)
                     .font(.title2)
                     .padding(.top)
